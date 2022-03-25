11d699df5f15e521d2a59a2b6df60ac748a82391
Laura Travin <laura.travin@gmail.com>
2022-03-24 20:16:06 -0400

    buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, AppBar , Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar,Container } from '@material-ui/core'
+import { Typography, AppBar , Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar,Container } from '@material-ui/core'
 import { PhotoCamera } from "@material-ui/icons";
 
 const App = () =>{
@@ -24,8 +24,21 @@ const App = () =>{
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                           when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
                         </Typography>
+                        <div>
+                            <Grid container spacing={2} justify="center">
+                                    <Grid item>
+                                        <Button variant="contained" color="primary">
+                                                See my photos
+                                        </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant="contained" color="primary">
+                                                Contact
+                                        </Button>
+                                    </Grid>
+                            </Grid>
+                        </div>
                    </Container>
                </div>
            </main>
