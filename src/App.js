677e0328cd9f2f6e619b18290946b087b754b090
Laura Travin <laura.travin@gmail.com>
2022-03-28 23:11:46 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Typography, AppBar , Button, Card, CardActions, CardContent, CardMedia,
 import { PhotoCamera } from "@material-ui/icons";
 import useStyles from "./styles";
 
-
+const cards =[1,2,3,4,5,6,7,8,9]
 const App = () =>{
    const classes = useStyles();
    return (
@@ -11,7 +11,7 @@ const App = () =>{
            <CssBaseline />
            <AppBar position="relative">
                <Toolbar>
-                   <PhotoCamera/>
+                   <PhotoCamera className={classes.icon}/>
                    <Typography variant="h6">
                         Photo Album
                    </Typography>
@@ -28,7 +28,7 @@ const App = () =>{
                          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                           when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                         </Typography>
-                        <div>
+                        <div className={classes.button}>
                             <Grid container spacing={2} justify="center">
                                     <Grid item>
                                         <Button variant="contained" color="primary">
@@ -44,7 +44,44 @@ const App = () =>{
                         </div>
                    </Container>
                </div>
+               <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                        {cards.map((card)=>(
+                            <Grid item key={card} xs={12} sm={6} md={4}  >
+                             <Card className={classes.card}>
+                                    <CardMedia 
+                                    className={classes.cardMedia} 
+                                    image="http://source.unsplash.com/random"
+                                    title="image title"/>
+                                    <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom variant="h5">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. Add a text to this section according the image.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary">View</Button>
+                                        <Button size="small" color="primary">Edit</Button>
+                                    </CardActions>
+                               </Card>
+                              </Grid>
+                                              
+                         ))}
+                        
+                    </Grid>
+                    
+               </Container>
            </main>
+           <footer className={classes.footer}>
+                        <Typography variant="h6" align="center" gutterBottom>
+                                Footer
+                        </Typography>
+                        <Typography variant="subtitle1" align="center" color="textSecondary">
+                                blablablaba blabe bla
+                        </Typography>
+           </footer>
        </>
    )
 }
--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { findByLabelText } from "@testing-library/react";
 
 //this hook returns an object that contain our styles
 const useStyles = makeStyles(
@@ -9,7 +10,33 @@ const useStyles = makeStyles(
                 // background: "red",
                 padding: theme.spacing(8,0,6)
 
+            },
+            icon:{
+                marginRight: '20px',
+            },
+            button:{
+                marginTop: '40px'
+            },
+            cardGrid:{
+                padding:"20px 0"
+            },
+            card:{
+                height: '100%',
+                display:'flex',
+                flexDirection: 'column'
+            },
+            cardMedia: {
+                paddingTop: '56.25%'
+            },
+            cardContent: {
+                flexGrow: 1,
+            },
+            footer: {
+                backgroundColor: theme.palette.background.paper,
+                padding: '50px 0'
             }
+
+
     })
     )
 
